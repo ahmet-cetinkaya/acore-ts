e53406c0186f92e2e9fc6355a11d081320c95982
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2025-01-12 17:03:09 +0300

    feat(repository): enhance IListable, ICountable, IGettable, and IRemovable interfaces to support asynchronous predicates and sorting

--- repository/abstraction/IRepository.ts
+++ repository/abstraction/IRepository.ts
@@ -1,16 +1,28 @@
 import type PaginationResult from "../PaginationResult";
 
+export type SortDirection = "asc" | "desc";
+
+export interface Sort<T> {
+  key: keyof T;
+  direction: SortDirection;
+}
+
 export interface IListable<T> {
-  getAll(predicate?: (x: T) => boolean): Promise<T[]>;
-  getList(pageIndex: number, pageSize: number, predicate?: (x: T) => boolean): Promise<PaginationResult<T>>;
+  getAll(predicate?: (x: T) => boolean | Promise<boolean>, sort?: Sort<T> | Sort<T>[]): Promise<T[]>;
+  getList(
+    pageIndex: number,
+    pageSize: number,
+    predicate?: (x: T) => boolean | Promise<boolean>,
+    sort?: Sort<T> | Sort<T>[],
+  ): Promise<PaginationResult<T>>;
 }
 
 export interface ICountable<T> {
-  count(predicate?: (x: T) => boolean): Promise<number>;
+  count(predicate?: (x: T) => boolean | Promise<boolean>): Promise<number>;
 }
 
 export interface IGettable<T> {
-  get(predicate: (x: T) => boolean): Promise<T | null>;
+  get(predicate: (x: T) => boolean | Promise<boolean>): Promise<T | null>;
 }
 
 export interface IAddable<T> {
@@ -22,7 +34,7 @@ export interface IUpdatable<T> {
 }
 
 export interface IRemovable<T> {
-  remove(predicate: (x: T) => boolean): Promise<void>;
+  remove(predicate: (x: T) => boolean | Promise<boolean>): Promise<void>;
 }
 
 export interface IRepository<T>
